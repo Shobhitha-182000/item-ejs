35cfbea5e47def735d7706dbbefa7caf5d43b06a
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-07-26 10:22:56 +0530

    item expense

---
+++ routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+//order is also important
+router.get('/products/:productId',shopController.getOneProduct)
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
